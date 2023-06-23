a4da8ff5e77de90fcaef2609c23116aba028bdff
Anh <vuonga1103@yahoo.com>
2023-06-23 14:32:17 -0400

    Change logo

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -64,9 +64,8 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'My Site',
         logo: {
-          alt: 'My Site Logo',
+          alt: 'Open Sprints Logo',
           src: 'img/logo.svg',
         },
         items: [
