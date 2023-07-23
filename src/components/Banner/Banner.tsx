61fe5f87c113cf3f8a6a84c5263587fa89bae128
Anh <vuonga1103@yahoo.com>
2023-07-23 16:17:43 -0400

    Build banner

---
+++ src/components/Banner/Banner.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import styles from './Banner.module.css';
+
+const Banner = ({ goalStatement, projectTags }) => {
+    const tags = projectTags.map(tag => (
+        <li key={tag}>#{tag}</li>
+    ))
+
+    return (
+        <div className={styles.container}>{goalStatement}
+            <ul className={styles.tagList}>
+                {tags}
+            </ul>
+        </div>
+    )
+}
+
+export default Banner
