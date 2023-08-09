a31241a3804ed66fafc5df626a276c8b70ad3d8c
Anh <vuonga1103@yahoo.com>
2023-08-08 21:57:42 -0400

    Build search projects banner

---
+++ src/components/SearchProjectsBanner/SearchProjectsBanner.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styles from './SearchProjectsBanner.module.css';
+
+const Banner = () => {
+
+
+    return (
+        <div className={styles.container}>
+            Search Projects
+        </div>
+    )
+}
+
+export default Banner
