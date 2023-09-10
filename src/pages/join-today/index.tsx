e99d2038292a9facdd394e03b87ad3b50d7752b1
Anh <vuonga1103@yahoo.com>
2023-09-10 16:33:11 -0400

    Add Airtable join form

--- src/pages/join-today/index.tsx
+++ src/pages/join-today/index.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import Layout from '@theme/Layout'
 
+const FORM_EMBED_LINK = 'https://airtable.com/embed/appJBLJKp4uAkZ1cu/shrT0JCWjsFMPJiYy'
+
 export default function JoinToday(): JSX.Element {
   return (
     <Layout>
-      <main>JoinToday Page</main>
+      <main>
+        <script src="https://static.airtable.com/js/embed/embed_snippet_v1.js"></script>
+        <iframe src={FORM_EMBED_LINK} width="100%" height="1963" ></iframe>
+      </main>
     </Layout>
   )
 }
