7cede10a82a90dca39cf3dbd694e35e90a01ef13
cfernando415 <ca.fernando415@gmail.com>
2023-11-03 17:05:25 +0000

    deploy: 698ec6d81889cabf4d4fecbe68d697b59775c447

--- assets/js/5d4484e3.cc0526e5.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkopen_sprints=self.webpackChunkopen_sprints||[]).push([[3954],{7055:s=>{s.exports=JSON.parse('{"permalink":"/open-sprints/blog/tags/docusaurus","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}')}}]);
